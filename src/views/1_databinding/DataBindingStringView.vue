fdf57c88df8c0987be9620c2e4971a1305149819
jeonga-Son <jjeong0106@gmail.com>
2023-03-20 23:26:37 +0900

    Data binding test

---
+++ src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Hello {{ userName }}</h1>
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line
+/* eslint-disable */
+export default {
+    // eslint-disable-next-line
+    /* eslint-disable */
+    data() {
+        // eslint-disable-next-line
+        /* eslint-disable */
+        return {
+            // eslint-disable-next-line
+            /* eslint-disable */
+            userName: 'Jone Doe',
+            message: 'Welcome 개발자의 품격',
+            arr: [],
+            obj: {}
+        }
+    }
+}
+</script>
+
+<style scoped>
+.text-red {
+    color: red;
+}
+</style>
