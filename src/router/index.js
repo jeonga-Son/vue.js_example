61ac804a16096a90fe68256208766b65733c2cc2
jeonga-Son <jjeong0106@gmail.com>
2023-03-21 06:46:19 +0900

    add input box to DataBindingInputView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,11 @@ const routes = [
     path: '/databinding/html',
     name: 'DataBindingHtmlView',
     component: () => import(/* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingHtmlView.vue')
+  },
+  {
+    path: '/databinding/input',
+    name: 'DataBindingInputView',
+    component: () => import(/* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingInputView.vue')
   }
 ]
 
